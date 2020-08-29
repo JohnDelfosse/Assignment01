7be4fd47ea0463bc0d9cc978c944e157d5e78927
Brock Melvin <bmelvin9@huskers.unl.edu>
2020-08-29 23:53:12 +0000

    included Brock Melvin as an author and created print statement with my name

--- hello.c
+++ hello.c
@@ -1,9 +1,9 @@
 /**
- * @author John Delfosse
+ * Authors: John Delfosse & Brock Melvin
  *
  * Date: 2020/08/29
  *
- * A simple hello world program in C
+ * A simple hello world program in C that prints the authors' names
  *
  */
 #include <stdlib.h>
@@ -12,6 +12,7 @@
 int main(int argc, char **argv) {
 
   printf("John Delfosse!\n");
+  printf("Brock Melvin!\n");
 
   return 0;
 }
