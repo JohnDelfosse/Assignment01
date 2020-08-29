c3c6940f33ea1a55e30f7a50cd751aede43e2d88
John Delfosse <jdelfosse2@huskers.unl.edu>
2020-08-29 23:38:47 +0000

    initial commint

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * @author John Delfosse
+ *
+ * Date: 2020/08/29
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("John Delfosse!\n");
+
+  return 0;
+}
